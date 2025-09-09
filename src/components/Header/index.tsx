83223379cc50d3205a7e396295ad00af139ac98d
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-09-09 11:16:28 -0300

    feat(header): adiciona modal de nova transação usando radix-ui/dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from "@radix-ui/react-dialog";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/logo.svg'
 export function Header() {
@@ -5,7 +6,31 @@ export function Header() {
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt="" />
-                <NewTransactionButton>Nova transações</NewTransactionButton>
+
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                    <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay />
+                        <Dialog.Content >
+                            <Dialog.Title>Nova transação</Dialog.Title>
+                            <Dialog.Description>
+                                Faça sua transação de forma rápida e segura
+                            </Dialog.Description>
+                            <form>
+                                <input type="text" placeholder="Descrição" />
+                                <input type="number" placeholder="Preço" />
+                                <input type="text" placeholder="Categoria" />
+                            </form>
+                            <Dialog.Close />
+                        </Dialog.Content>
+                    </Dialog.Portal>
+
+                </Dialog.Root>
+
             </HeaderContent>
         </HeaderContainer>
     )
