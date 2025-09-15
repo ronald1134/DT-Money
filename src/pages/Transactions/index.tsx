1ba7803394d497f8c219141391e86d67c823a8ab
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-09-15 14:09:19 -0300

    feat: adiciona requisição HTTP para buscar transações e renderizar na tabela

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+    id: number;
+    description: string;
+    type: 'income' | 'outcome';
+    price: number;
+    category: string;
+    createdAt: string;
+}
+
 export function Transactions() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    async function loadTransactions() {
+        const response = await fetch("http://localhost:3333/transactions")
+        const data = await response.json()
+        setTransactions(data)
+    }
+
+    useEffect(() => {
+        loadTransactions();
+    }, [])
+
     return (
         <div>
             <Header />
@@ -13,58 +35,24 @@ export function Transactions() {
                 <SearchForm />
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td>Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>20/09/2025</td>
-                        </tr>
-                        <tr>
-                            <td>Hamburguer</td>
-                            <td>
-                                <PriceHighlight variant="outcome">
-                                    - R$ 59,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>10/04/2025</td>
-                        </tr>
-                        <tr>
-                            <td>Aluguel do apartamento</td>
-                            <td>
-                                <PriceHighlight variant="outcome">
-                                    - R$ 1.200,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Casa</td>
-                            <td>27/03/2025</td>
-                        </tr>
-
-                        <tr>
-                            <td>Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    R$ 8.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/03/2025</td>
-                        </tr>
-                        
-                        <tr>
-                            <td>Salário</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    R$ 5.400,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Salário</td>
-                            <td>05/09/2025</td>
-                        </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighlight variant={transaction.type}>
+                                            {transaction.type === 'outcome' && '- '}
+                                            {new Intl.NumberFormat('pt-BR', {
+                                                style: 'currency',
+                                                currency: 'BRL'
+                                            }).format(transaction.price)}
+                                        </PriceHighlight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
