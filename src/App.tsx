854da6a4b7b4920ea0b915ecfb52f844cc5e7d86
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-09-16 11:46:45 -0300

    feat: Adiciona contextprovider de transações no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./Styles/themes/default"
 import { GlobalStyle } from "./Styles/global"
 import { Transactions } from "./pages/Transactions"
+import { TransactionsProvider } from "./contexts/TransactionsContext"
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-    
-      <Transactions />      
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
