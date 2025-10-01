77f83ca67cca4f0b06c640792f97df26612d1ae5
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-10-01 12:20:36 -0300

    feat(transactions): permite filtrar transações pelo termo digitado

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -13,6 +13,7 @@ interface Transaction {
 
 interface TransactionContextType {
     transactions: Transaction[];
+    fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
@@ -24,19 +25,28 @@ export const TransactionsContext = createContext({} as TransactionContextType);
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-    async function Transactions() {
-        const response = await fetch("http://localhost:3333/transactions")
+    async function fetchTransactions(query?: string) {
+
+        const url = new URL('http://localhost:3333/transactions');
+        if (query) {
+            url.searchParams.append('q', query);
+        }
+
+        const response = await fetch(url)
         const data = await response.json()
         setTransactions(data)
     }
 
     useEffect(() => {
-        Transactions();
+        fetchTransactions();
     }, [])
 
     return (
-        <TransactionsContext.Provider value={{ transactions }}>
+        <TransactionsContext.Provider value={{
+            transactions,
+            fetchTransactions
+        }}>
             {children}
         </TransactionsContext.Provider>
-    )   
+    )
 }
