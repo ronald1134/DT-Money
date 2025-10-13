853e08d433d450a0cba33bad7b7776906076bc3d
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-10-13 10:09:31 -0300

    (Summary) aplicando o useMemo para evitar renderização de variaveis e otimizando

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { TransactionsContext } from "../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
+import { useMemo } from "react";
 
 export function useSummary() {
     const  transactions  = useContextSelector(
@@ -11,24 +12,26 @@ export function useSummary() {
 
     console.log(transactions);
 
-    const summary = transactions.reduce(
-        (acc, transaction) => {
-            if (transaction.type === 'income') {
-                acc.income += transaction.price;
-                acc.total += transaction.price;
-            } else {
-                acc.outcome += transaction.price;
-                acc.total -= transaction.price;
+    const summary = useMemo(() => { // memoiza o calculo, so recalcula quando transactions mudar
+        return transactions.reduce(
+            (acc, transaction) => {
+                if (transaction.type === 'income') {
+                    acc.income += transaction.price;
+                    acc.total += transaction.price;
+                } else {
+                    acc.outcome += transaction.price;
+                    acc.total -= transaction.price;
+                }
+                acc.total = acc.income - acc.outcome;
+                return acc;
+            },
+            {
+                income: 0,
+                outcome: 0,
+                total: 0
             }
-            acc.total = acc.income - acc.outcome;
-            return acc;
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0
-        }
-    )
+        )
+    }, [transactions]);
 
     return summary;
 }
